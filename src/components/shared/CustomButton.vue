fb75a01616122f14fb7ac4e48c1c487dc0c76d21
unknown <guillotboris@hotmail.com>
2025-08-05 10:18:58 +0200

    fix/interface

--- src/components/shared/CustomButton.vue
+++ src/components/shared/CustomButton.vue
@@ -1,11 +1,20 @@
 <template>
-    <button
-        :class="'btn ' + (obj.classNames ?? 'btn-light')" 
-        :id="obj.id ?? ''" :type="obj.type ?? 'button'"
-    >{{ obj.content }}</button>
+  <button
+    :class="'btn ' + (obj.classNames ?? 'btn-light')"
+    :id="obj.id ?? ''"
+    :type="obj.type ?? 'button'"
+  >
+    {{ obj.content }}
+  </button>
 </template>
 
 <script setup lang="ts">
-import { CustomButtonInterface, type CustomButtonHandlerType  } from "../../interfaces/shared-interfaces/CustomButtonInterface"
-defineProps<{ obj: CustomButtonInterface & CustomButtonHandlerType  , key: number}>()
+import type {
+  CustomButtonInterface,
+  CustomButtonHandlerType,
+} from "../../interfaces/shared-interfaces/CustomButtonInterface";
+defineProps<{
+  obj: CustomButtonInterface & CustomButtonHandlerType;
+  key: number;
+}>();
 </script>
--- src/components/shared/CustomForm.vue
+++ src/components/shared/CustomForm.vue
@@ -1,39 +1,47 @@
 <template>
-
-    <form class="col-12 col-xl-3 col-md-6 col-sm-8" @submit.prevent="onSubmit" @reset="onReset">
-        <section>
-            <FormTextField 
-                @on-input-updated="onFieldUpdated" 
-                v-for="(field,i) in fields" 
-                :key="i" 
-                :obj="field"
-            />
-        </section>
-        <section>
-            <CustomButton v-for="(btn,i) in buttons" :obj="btn" :key="i"/>
-        </section>
-        <section class="my-3 d-flex flex-column" style="transition: height 300ms ease-in-out;">              
-            <CustomMessage v-for="(message,i) in messages" :key="i" :obj="message" v-if="messages && messages.length > 0"  />
-        </section>
-    </form>
-
-
+  <form
+    class="col-12 col-xl-3 col-md-6 col-sm-8"
+    @submit.prevent="onSubmit"
+    @reset="onReset"
+  >
+    <section>
+      <FormTextField
+        @on-input-updated="onFieldUpdated"
+        v-for="(field, i) in fields"
+        :key="i"
+        :obj="field"
+      />
+    </section>
+    <section>
+      <CustomButton v-for="(btn, i) in buttons" :obj="btn" :key="i" />
+    </section>
+    <section
+      class="my-3 d-flex flex-column"
+      style="transition: height 300ms ease-in-out"
+    >
+      <CustomMessage
+        v-for="(message, i) in messages"
+        :key="i"
+        :obj="message"
+        v-if="messages && messages.length > 0"
+      />
+    </section>
+  </form>
 </template>
 <script lang="ts" setup>
 import FormTextField from "../form-components/FormTextField.vue";
 import CustomButton from "./CustomButton.vue";
 import CustomMessage from "./CustomMessage.vue";
-import { CustomMessageInterface } from "../../interfaces/shared-interfaces/CustomMessageInterface";
-import { type CustomFormHandlersType, CustomFormInterface } from "../../interfaces/shared-interfaces/CustomFormInterface";
+import type { CustomMessageInterface } from "../../interfaces/shared-interfaces/CustomMessageInterface";
+import type {
+  CustomFormHandlersType,
+  CustomFormInterface,
+} from "../../interfaces/shared-interfaces/CustomFormInterface";
 
-defineProps<CustomFormInterface & CustomFormHandlersType>()
-const emit = defineEmits(["onFieldUpdated"])
+defineProps<CustomFormInterface & CustomFormHandlersType>();
+const emit = defineEmits(["onFieldUpdated"]);
 
 const onFieldUpdated = (obj: any) => {
-    emit("onFieldUpdated", obj)
-}
-
-
+  emit("onFieldUpdated", obj);
+};
 </script>
-
-
