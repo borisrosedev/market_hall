7c184ef847f39b6c72d99f747a3fa3595dc47717
GuillotBoris <guillotboris@hotmail.com>
2025-08-22 16:18:10 +0200

    feature-landing-page-carousel-gasp-animate

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+  
   <ToastOverlay />
   <NavBar />
   <RouterView />
   <FooterBar />
 </template>
 <script setup lang="ts">
+import 'animate.css'; 
 import NavBar from "./layout/NavBar.vue";
 import ToastOverlay from "./layout/ToastOverlay.vue";
 import FooterBar from "./layout/FooterBar.vue";
