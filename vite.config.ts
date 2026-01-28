2b32fd2104d510383a96589fa376493df2e8bd6f
mahmoud <mahmoudyaya33@gmail.com>
2026-01-28 10:54:45 +0100

    Revert "Modifié un fichier setup pour ajouter Vitest dans github actions"
    This reverts commit b723df1c9d78f55590f69626b1debd35b6d479d8.

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,26 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   test: {
+    setupFiles: "./vitest.setup.ts",
+    // enable jest-like global test APIs
     globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
     environment: "happy-dom",
     coverage: {
       reporter: ["text", "json", "html"],
     },
-    setupFiles: ["./vitest.setup.ts"],
   },
   server: {
     host: "0.0.0.0",
     port: 5173,
     strictPort: true,
     watch: {
-      usePolling: true,
+      usePolling: true, // Pour Windows/WSL
     },
   },
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,6 +3,3 @@ import { webcrypto } from "node:crypto";
 Object.defineProperty(global.self, "crypto", {
   value: webcrypto,
 });
-if (typeof global.crypto === "undefined") {
-  global.crypto = webcrypto as any;
-}
